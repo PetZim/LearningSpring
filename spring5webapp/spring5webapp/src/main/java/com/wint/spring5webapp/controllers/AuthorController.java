f858d9a9c1e64850c75a0e2fb8e7c2a2c86265ed
LAPTOP-K41JB88F\peter <peterkoziman>
2018-06-10 17:44:23 +0200

    Added AuthorController and thymeleaf template authors.html

---
+++ spring5webapp/spring5webapp/src/main/java/com/wint/spring5webapp/controllers/AuthorController.java
@@ -0,0 +1,25 @@
+package com.wint.spring5webapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wint.spring5webapp.repositories.AuthorRepository;
+
+@Controller
+public class AuthorController 
+{
+	private AuthorRepository authorRepository;
+
+	public AuthorController(AuthorRepository authorRepository) {
+		super();
+		this.authorRepository = authorRepository;
+	}
+	
+	@RequestMapping("/authors")
+	public String getAuthors(Model model) {
+		model.addAttribute("authors", authorRepository.findAll());
+		return "authors";
+	}
+	
+}
