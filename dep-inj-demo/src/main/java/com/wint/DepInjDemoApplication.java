7906a1db3e4e1f83fdf6b7a4a1ae90e517c03d12
LAPTOP-K41JB88F\peter <peterkoziman>
2018-06-13 21:22:32 +0200

    Simple HelloWorld

--- dep-inj-demo/src/main/java/com/wint/DepInjDemoApplication.java
+++ dep-inj-demo/src/main/java/com/wint/DepInjDemoApplication.java
@@ -1,12 +1,18 @@
 package com.wint;
 
+import org.omg.CORBA.CTX_RESTRICT_SCOPE;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import com.wint.controllers.MyController;
 
 @SpringBootApplication
 public class DepInjDemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DepInjDemoApplication.class, args);
+		ApplicationContext context = SpringApplication.run(DepInjDemoApplication.class, args);
+		MyController controller = (MyController) context.getBean("myController");
+		controller.hello();
 	}
 }
