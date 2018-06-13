56beb3e14a6e4730f9d3a8841d8cc796ebbad05d
LAPTOP-K41JB88F\peter <peterkoziman>
2018-06-13 20:48:47 +0200

    Initial

---
+++ dep-inj-demo/src/main/java/com/wint/DepInjDemoApplication.java
@@ -0,0 +1,12 @@
+package com.wint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepInjDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepInjDemoApplication.class, args);
+	}
+}
