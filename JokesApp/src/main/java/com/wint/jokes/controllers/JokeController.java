2071ba498a3b64b6cbbfe981a67e724d148146cd
LAPTOP-K41JB88F\peter <peterkoziman>
2018-06-23 15:50:40 +0200

    Creating the View Layer

--- JokesApp/src/main/java/com/wint/jokes/controllers/JokeController.java
+++ JokesApp/src/main/java/com/wint/jokes/controllers/JokeController.java
@@ -3,10 +3,32 @@
  */
 package com.wint.jokes.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.wint.jokes.services.JokeService;
+
 /**
  * @author peter
  *
  */
+@Controller
 public class JokeController {
+	
+	private JokeService jokeService;
 
+	@Autowired
+	public JokeController(JokeService jokeService) {
+		super();
+		this.jokeService = jokeService;
+	}
+	
+	@RequestMapping({"/", ""})
+	public String showJoke(Model model) {
+		model.addAttribute("joke", jokeService.getJoke());
+		return "chucknorris";
+	}
+	
 }
--- JokesApp/src/main/java/com/wint/jokes/services/JokeServiceImpl.java
+++ JokesApp/src/main/java/com/wint/jokes/services/JokeServiceImpl.java
@@ -3,32 +3,30 @@
  */
 package com.wint.jokes.services;
 
+import org.springframework.stereotype.Service;
+
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
 
 /**
  * @author peter
  *
  */
+@Service
 public class JokeServiceImpl implements JokeService {
 
 	private final ChuckNorrisQuotes chuckNorrisQuotes;
-	
-	
 	
 	public JokeServiceImpl() {
 		super();
 		this.chuckNorrisQuotes = new ChuckNorrisQuotes();
 	}
 
-
-
 	/* (non-Javadoc)
 	 * @see com.wint.jokes.services.JokeService#getJoke()
 	 */
 	@Override
 	public String getJoke() {
-		
-		return null;
+		return chuckNorrisQuotes.getRandomQuote();
 	}
 
 }
